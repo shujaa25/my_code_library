71e4c7962e9a8d563b20b7c8505fa8fc3c9ff7e0
Shujaa Ahmad <shujaa25@gmail.com>
2022-10-21 19:30:49 +0530

    updated cycle sort tests

--- tests/TestCycleSort.java
+++ tests/TestCycleSort.java
@@ -17,5 +17,9 @@ public class TestCycleSort extends TestCase {
         int[] a = {2,4,5,1,3, 9, 10, 6, 8, 7, 0};
         cycleSort.sortDistinct(a);
         Assert.assertArrayEquals(a, new int[]{0,1,2,3,4,5,6,7,8,9,10});
+
+        int[] b = {0,1,0,1,0,1,0,0,0,1,1,0,2};
+        cycleSort.sort(b);
+        Assert.assertArrayEquals(b, new int[]{0,0,0,0,0,0,0,1,1,1,1,1,2});
     }
 }
